48eba46c490cdd672d8bba9a26687dffc4170bcc
Shir Bar Lev <shirblc@gmail.com>
2020-12-02 20:23:35 +0000

    Added the redux store to the app.

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Provider store={store}>
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({
