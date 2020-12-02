0c542bd796b056596f92c22e0136cf1df4d6ff53
Shir Bar Lev <shirblc@gmail.com>
2020-12-02 20:26:04 +0000

    Turned App Component into a class based component.

--- App.js
+++ App.js
@@ -8,15 +8,18 @@ import reducer from './reducers/index';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
-export default function App() {
-	return (
-		<Provider store={store}>
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
-		</Provider>
-	);
+export default class App extends React.Component {
+	// render
+	render() {
+		return (
+			<Provider store={store}>
+				<View style={styles.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+					<StatusBar style="auto" />
+				</View>
+			</Provider>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
