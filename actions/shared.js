ea4a1d56b7893abfc6f4ffe00f967adafb3b9e71
Shir Bar Lev <shirblc@gmail.com>
2020-12-02 20:46:19 +0000

    Added missing parentheses to action.

--- actions/shared.js
+++ actions/shared.js
@@ -5,7 +5,7 @@ import { addQuestions } from './questions';
 // Get the initial data from AsyncStorage
 export function getInitialData() {
 	return (dispatch) => {
-		return getData().then({ decks, questions } => {
+		return getData().then(({ decks, questions }) => {
 			dispatch(addDecks(decks));
 			dispatch(addQuestions(questions));
 		})
