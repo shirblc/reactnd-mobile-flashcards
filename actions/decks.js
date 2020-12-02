8edd8b6fbe636d47e08a1da0af01320cc63169b7
Shir Bar Lev <shirblc@gmail.com>
2020-12-02 21:04:35 +0000

    Added functionality for adding a deck.

--- actions/decks.js
+++ actions/decks.js
@@ -1,9 +1,21 @@
 export const ADD_DECKS = 'ADD_DECKS';
+export const CREATE_DECK = 'CREATE_DECK';
 
 // Add multiple decks to the redux store; is triggered by initial data fetch
 export function addDecks(decks) {
 	return {
 		type: ADD_DECKS,
 		decks
+	}
+}
+
+// Create a new deck
+export function createDeck(deckName) {
+	return {
+		type: createDeck,
+		deck: {
+			name: deckName,
+			questions: []
+		}
 	}
 }
--- reducers/decks.js
+++ reducers/decks.js
@@ -1,4 +1,4 @@
-import { ADD_DECKS } from '../actions/decks';
+import { ADD_DECKS, CREATE_DECK } from '../actions/decks';
 
 // Decks reducer
 export default function decksReducer(state = {}, action) {
@@ -6,6 +6,16 @@ export default function decksReducer(state = {}, action) {
 		// if the incoming action is to add multiple decks, add them to the current state
 		case ADD_DECKS:
 			return { ...state, ...action.decks }
+		// if the incoming action is to create a deck, add it to the current state
+		case CREATE_DECK:
+			// add 1 to the last deck's ID or if there's no deck, make the ID 0
+			const id = Object.keys(state).length ? (Object.keys(state)[Object.keys(state).length-1] + 1) : 0;
+			return {
+				...state,
+				[id]: {
+					...action.deck
+				}
+			}
 		// otherwise return the state as is
 		default:
 			return state
