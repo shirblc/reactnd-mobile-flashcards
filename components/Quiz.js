b87447fd4a7ea74dceaf1d721084aa10a7874723
Shir Bar Lev <shirblc@gmail.com>
2020-12-03 14:39:17 +0000

    Added dynamic question/answer view to Quiz.
    Now the user can choose to view either the question or the answer. A click on the button changes both the button text and the question/answer text being displayed.

--- components/Quiz.js
+++ components/Quiz.js
@@ -5,7 +5,17 @@ import { connect } from 'react-redux';
 class Quiz extends React.Component {
 	state = {
 		correctAnswers: 0,
-		currentQuestion: 0
+		currentQuestion: 0,
+		currentlyShowing: 'question',
+		otherOption: 'answer'
+	}
+
+	// Change the text currently showing; if the user's viewing the question, show the answer, and vice versa
+	changeView() {
+		this.setState(currentState => ({
+			currentlyShowing: currentState['currentlyShowing'] === 'question' ? 'answer' : 'question',
+			otherOption: currentState['otherOption'] === 'answer' ? 'question' : 'answer'
+		}))
 	}
 
 	// render method
@@ -13,8 +23,8 @@ class Quiz extends React.Component {
 		return (
 			<View>
 				<Text>{ this.state.currentQuestion + 1 } / { this.props.questions.length }</Text>
-				<Text>{ this.props.questions[this.state.currentQuestion].question }</Text>
-				<TouchableOpacity><Text>Click to view the answer</Text></TouchableOpacity>
+				<Text>{ this.props.questions[this.state.currentQuestion][this.state.currentlyShowing] }</Text>
+				<TouchableOpacity onPress={() => (this.changeView())}><Text>Click to view the { this.state.otherOption }</Text></TouchableOpacity>
 				
 				<TouchableOpacity><Text>Correct</Text></TouchableOpacity>
 				<TouchableOpacity><Text>Incorrect</Text></TouchableOpacity>
