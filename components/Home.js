0efdeb18df2b68a0b7f4303f70cf62f9c96378b2
Shir Bar Lev <shirblc@gmail.com>
2020-12-03 10:16:23 +0000

    Added button to create a new deck.

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { connect } from 'react-redux';
+import { AntDesign } from '@expo/vector-icons'; 
 import { getInitialData } from '../actions/shared';
 import Deck from './Deck';
 
@@ -14,6 +15,9 @@ class Home extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
+				<Pressable style={styles.deck}>
+					<AntDesign name="plus" size={24} color="black" />
+				</Pressable>
 				{
 					Object.values(this.props.decks).length
 					? Object.values(this.props.decks).map(deck =>  <Deck deckName={deck.name} cardsNumber={deck.questions.length} />)
@@ -34,12 +38,20 @@ function mapStateToProps({ decks }) {
 
 // Styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	deck: {
+		borderColor: '#000',
+		borderStyle: 'solid',
+		borderRadius: 5,
+		borderWidth: 3,
+		padding: 10,
+		margin: 5
+	},
 });
 
 export default connect(mapStateToProps)(Home)
