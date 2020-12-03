1061d2833dacc928e237237e8af646910e1bdd59
Shir Bar Lev <shirblc@gmail.com>
2020-12-03 16:37:33 +0000

    Deleted unneeded import.

--- components/Deck.js
+++ components/Deck.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 
 // Deck of cards
 class Deck extends React.Component {
