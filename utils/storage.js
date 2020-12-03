330e5e16cb833674778bff33b75ffed8d5d06e34
Shir Bar Lev <shirblc@gmail.com>
2020-12-03 10:27:48 +0000

    Moved Deck and Question IDs to the utils file.

--- utils/storage.js
+++ utils/storage.js
@@ -1,10 +1,18 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STORAGE_KEY = 'mobileFlashcards';
+let NEXT_DECK_ID = 0;
+let NEXT_QUESTION_ID = 0;
 
 // Get all saved data from AsyncStorage
 export function getData() {
 	return AsyncStorage.getItem(STORAGE_KEY).then(data => {
+		// if there's any existing data, update the value for the question and deck added next
+		if(data) {
+			NEXT_DECK_ID = Number(Object.keys(data.decks)[Object.keys(data.decks).length-1] + 1);
+			NEXT_QUESTION_ID = Number(Object.keys(data.questions)[Object.keys(data.questions).length-1] + 1);
+		}
+		// if there's any existing data, return it; otherwise return empty objects
 		return data ? { decks, questions } : { decks: {}, questions: {} }
 	});
 }
