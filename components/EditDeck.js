eb06e8bd20e861487fe6ae2005d81c0086a7309b
Shir Bar Lev <shirblc@gmail.com>
2020-12-04 16:07:37 +0000

    Added styling for EditDeck component.

--- components/EditDeck.js
+++ components/EditDeck.js
@@ -5,15 +5,41 @@ import { connect } from 'react-redux';
 class EditDeck extends React.Component {
 	render() {
 		return (
-			<KeyboardAvoidingView>
-				<Text>Deck Name:</Text>
-				<TextInput></TextInput>
-				<TouchableOpacity><Text>Edit Deck Name</Text></TouchableOpacity>
-				<TouchableOpacity><Text>Delete Deck</Text></TouchableOpacity>
-				<TouchableOpacity><Text>Clear Deck</Text></TouchableOpacity>
+			<KeyboardAvoidingView style={styles.container}>
+				<Text style={styles.fieldTitle}>Deck Name:</Text>
+				<TextInput style={styles.textField}></TextInput>
+				<TouchableOpacity style={styles.submit}><Text>Edit Deck Name</Text></TouchableOpacity>
+				<TouchableOpacity style={styles.submit}><Text>Delete Deck</Text></TouchableOpacity>
+				<TouchableOpacity style={styles.submit}><Text>Clear Deck</Text></TouchableOpacity>
 			</KeyboardAvoidingView>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 5,
+		alignItems: 'center'
+	},
+	fieldTitle: {
+		fontSize: 20,
+		padding: 10
+	},
+	textField: {
+		height: 30,
+		width: 200,
+		borderWidth: 1,
+		borderColor: 'black',
+		padding: 5,
+		margin: 10
+	},
+	submit: {
+		padding: 5,
+		margin: 10,
+		borderWidth: 1,
+		borderColor: 'black'
+	}
+});
 
 export default connect()(EditDeck);
