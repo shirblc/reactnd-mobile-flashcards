5778a2c85569ccc8f42762c892b9be7e5af7b03f
Shir Bar Lev <shirblc@gmail.com>
2020-12-08 15:20:00 +0000

    Added actions for deleting a deck.

--- actions/decks.js
+++ actions/decks.js
@@ -1,8 +1,9 @@
-import { createDeck as createDeckInStorage, editDeckName } from '../utils/storage';
+import { createDeck as createDeckInStorage, editDeckName, deleteDeck as deleteDeckInStorage } from '../utils/storage';
 
 export const ADD_DECKS = 'ADD_DECKS';
 export const CREATE_DECK = 'CREATE_DECK';
 export const EDIT_DECK = 'EDIT_DECK';
+export const DELETE_DECK = 'DELETE_DECK';
 
 // Add multiple decks to the redux store; is triggered by initial data fetch
 export function addDecks(decks) {
@@ -44,5 +45,22 @@ function editDeck(updatedDeck, deckID) {
 		type: EDIT_DECK,
 		id: deckID,
 		updatedDeck
+	}
+}
+
+// Delete the given deck in async storage and then update it in the redux store
+export function deleteDeckAsync(deckID) {
+	return (dispatch) => {
+		deleteDeckInStorage(deckID).then(() => {
+			dispatch(deleteDeck(deckID))
+		})
+	}
+}
+
+// Delete the given deck
+function deleteDeck(deckID) {
+	return {
+		type: DELETE_DECK,
+		deckID
 	}
 }
